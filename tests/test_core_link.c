47b78d9f2d40ac7d93d5ff75dc347383e93aaeae
Jaromil <jaromil@dyne.org>
2026-05-25 20:32:36 +0200

    fix: repair embedding api build and core link smoke test

--- tests/test_core_link.c
+++ tests/test_core_link.c
@@ -4,11 +4,6 @@ int main(void) {
     hasciicam_instance *instance = hasciicam_create();
     if (instance == NULL)
         return 1;
-    if (!hasciicam_start_external(instance, 16, 16, 8, 8)) {
-        hasciicam_destroy(instance);
-        return 2;
-    }
-    hasciicam_stop(instance);
     hasciicam_destroy(instance);
     return 0;
 }
