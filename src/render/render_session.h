216cb0e2afaf3b934753377ba5223817c0224467
Jaromil <jaromil@dyne.org>
2026-05-25 19:35:08 +0200

    refactor: move aalib setup into render session module

---
+++ src/render/render_session.h
@@ -0,0 +1,42 @@
+#ifndef HASCIICAM_RENDER_SESSION_H
+#define HASCIICAM_RENDER_SESSION_H
+
+#include <aalib.h>
+
+typedef struct hasciicam_render_session {
+    aa_context *context;
+    struct aa_hardware_params hwparams;
+    struct aa_renderparams *render_params;
+    struct aa_savedata save;
+    struct aa_format *html_format;
+    char html_header[1024];
+} hasciicam_render_session;
+
+void hasciicam_render_session_init(hasciicam_render_session *session);
+void hasciicam_render_session_configure_geometry(hasciicam_render_session *session,
+                                                 int rec_width,
+                                                 int rec_height);
+void hasciicam_render_session_prepare_html(hasciicam_render_session *session,
+                                           int refresh,
+                                           const char *aafile,
+                                           int linespace,
+                                           const char *background,
+                                           const char *foreground,
+                                           int fontsize,
+                                           const char *fontface);
+void hasciicam_render_session_configure_save(hasciicam_render_session *session,
+                                             int mode,
+                                             char *aafile,
+                                             char *tmpfile,
+                                             size_t tmpfile_size);
+int hasciicam_render_session_open(hasciicam_render_session *session,
+                                  int mode,
+                                  const char *aadriver,
+                                  int quiet);
+void hasciicam_render_session_apply_tuning(hasciicam_render_session *session,
+                                           int bright,
+                                           int contrast,
+                                           float gamma);
+void hasciicam_render_session_close(hasciicam_render_session *session);
+
+#endif
