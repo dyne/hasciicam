d40cf0e0348dbd021c24459b88854604c314d38b
Jaromil <jaromil@dyne.org>
2026-05-25 10:16:10 +0200

    refactor: make main loop backend-neutral

--- src/capture/capture_v4l2.h
+++ src/capture/capture_v4l2.h
@@ -1,18 +1,8 @@
 #ifndef HASCIICAM_CAPTURE_V4L2_H
 #define HASCIICAM_CAPTURE_V4L2_H
 
-struct geometry {
-    int w;
-    int h;
-    int size;
-    int bright;
-    int contrast;
-    int gamma;
-};
+#include "capture.h"
 
-int vid_detect(char *devfile);
-int vid_init(void);
-void grab_one(void);
-void vid_close(void);
+const capture_ops *capture_v4l2_ops(void);
 
 #endif
