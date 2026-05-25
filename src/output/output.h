5a59962083f0585369a17102dbc31f7b7879cd7c
Jaromil <jaromil@dyne.org>
2026-05-25 19:38:28 +0200

    refactor: add aalib-backed output port module

---
+++ src/output/output.h
@@ -0,0 +1,17 @@
+#ifndef HASCIICAM_OUTPUT_H
+#define HASCIICAM_OUTPUT_H
+
+#include <aalib.h>
+
+typedef struct hasciicam_output {
+    aa_context *context;
+    const char *adapter_name;
+} hasciicam_output;
+
+int hasciicam_output_open_aalib(hasciicam_output *output, aa_context *context);
+void hasciicam_output_write_ascii_frame(hasciicam_output *output, int ascii_width, int ascii_height);
+void hasciicam_output_poll(hasciicam_output *output);
+void hasciicam_output_close(hasciicam_output *output);
+const char *hasciicam_output_name(const hasciicam_output *output);
+
+#endif
