3d0780690b6e9ef0f848563f343b01d35300572e
Jaromil <jaromil@dyne.org>
2026-05-25 10:18:55 +0200

    feat: add media foundation backend skeleton

--- src/capture/capture_backend.c
+++ src/capture/capture_backend.c
@@ -1,6 +1,11 @@
 #include "capture_backend.h"
+#include "capture_mf.h"
 #include "capture_v4l2.h"
 
 const capture_ops *capture_default_ops(void) {
+#if defined(_WIN32)
+    return capture_mf_ops();
+#else
     return capture_v4l2_ops();
+#endif
 }
