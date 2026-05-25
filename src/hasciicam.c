daed22d7fe81707469768af718d84a0e64044f59
Jaromil <jaromil@dyne.org>
2026-05-25 10:11:54 +0200

    refactor: extract linux v4l2 capture adapter

--- src/hasciicam.c
+++ src/hasciicam.c
@@ -41,20 +41,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/time.h>
-#endif
-
-#if defined(__linux__)
-#define HASCIICAM_HAVE_V4L2 1
-#include <sys/mman.h>
-#include <sys/ioctl.h>
-#include <sys/types.h>
-#include <pwd.h>
 #include <grp.h>
-#include <linux/types.h>
-#include <linux/videodev2.h>
 #endif
 
 #include <aalib.h>
+#include "capture/capture_v4l2.h"
 
 /* hasciicam modes */
 #define LIVE 0
@@ -131,9 +122,6 @@ int inputch = 0;
 int daemon_mode = 0;
 int invert = 0;
 
-struct geometry {
-  int w, h, size;
-  int bright, contrast, gamma; };
 struct geometry aa_geo;
 struct geometry vid_geo;
 /* if width&height have been manually changed */
@@ -195,24 +183,6 @@ struct aa_format hascii_format = {
   html_escapes
 };
 
-#if defined(HASCIICAM_HAVE_V4L2)
-/* v4l2 (thanks Dan)*/
-int buftype = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-struct v4l2_capability capability;
-struct v4l2_input input;
-struct v4l2_standard standard;
-struct v4l2_format format;
-struct v4l2_requestbuffers reqbuf;
-struct v4l2_buffer buffer;
-struct {
-    void *start;
-    size_t length;
-} *buffers;
-
-
-int fd = -1;
-#endif
-
 /* greyscale image is sampled from Y luminance component */
 unsigned char *grey;
 int YtoRGB[256];
@@ -271,249 +241,6 @@ void YUV422_to_grey_scaled(unsigned char *src, unsigned char *dst, int src_w, in
         }
     }
 }
-
-#if defined(HASCIICAM_HAVE_V4L2)
-int vid_detect(char *devfile) {
-    int errno;
-    unsigned int i;
-
-    if (-1 == (fd = open(devfile,O_RDWR|O_NONBLOCK))) {
-        perror("!! error in opening video capture device: ");
-        return -1;
-    } else {
-        close(fd);
-        fd = open(devfile,O_RDWR);
-    }
-
-// Check that streaming is supported
-    memset(&capability, 0, sizeof(capability));
-    if(-1 == ioctl(fd, VIDIOC_QUERYCAP, &capability)) {
-	perror("VIDIOC_QUERYCAP");
-	exit(EXIT_FAILURE);
-    }
-    if((capability.capabilities & V4L2_CAP_VIDEO_CAPTURE) == 0){
-	printf("Fatal: Device %s does not support video capture\n", capability.card);
-	exit(EXIT_FAILURE);
-    }
-    if((capability.capabilities & V4L2_CAP_STREAMING) == 0){
-	printf("Fatal: Device %s does not support streaming data capture\n", capability.card);
-	exit(EXIT_FAILURE);
-    }
-
-    fprintf(stderr,"Device detected is %s\n",devfile);
-    fprintf(stderr,"Card name: %s\n",capability.card);
-
-// Switch to the selected video input
-    if(-1 == ioctl(fd, VIDIOC_S_INPUT, &inputch)) {
-	perror("VIDIOC_S_INPUT");
-	exit(EXIT_FAILURE);
-    }
-
-// Get info about current video input
-    memset(&input, 0, sizeof(input));
-    input.index = inputch;
-    if(-1 == ioctl(fd, VIDIOC_ENUMINPUT, &input)) {
-	perror("VIDIOC_ENUMINPUT");
-	exit(EXIT_FAILURE);
-    }
-    printf("Current input is %s\n", input.name);
-// example 1-6
-    memset(&standard, 0, sizeof(standard));
-    standard.index = 0;
-    while(0 == ioctl (fd, VIDIOC_ENUMSTD, &standard)){
-	if(standard.id & input.std)
-            printf("   - %s\n", standard.name);
-	standard.index++;
-    }
-// Need to find out (request?) specific data format (sec 1.10.1)
-    memset(&format, 0, sizeof(format));
-    format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    /* the format.fmt.pix member, a v4l2_pix_format, is now filled in
-       with default falues */
-    if(-1 == ioctl(fd, VIDIOC_G_FMT, &format)) {
-      perror("VIDIOC_G_FMT");
-      exit(EXIT_FAILURE);
-    }
-
-    format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
-
-    if(whchanged==1) {
-    // fill in user defined values
-     fprintf(stderr,"user defined size: %u x %u\n", user_w, user_h);
-     format.fmt.pix.width  = user_w;
-     format.fmt.pix.height = user_h;
-    }
-
-    if(-1 == ioctl(fd, VIDIOC_S_FMT, &format)) {
-      perror("VIDIOC_S_FMT");
-      exit(EXIT_FAILURE);
-    }
-
-    printf("Current capture is %u x %u\n",
-           format.fmt.pix.width, format.fmt.pix.height);
-    printf("format %4.4s, %u bytes-per-line\n",
-           (char*)&format.fmt.pix.pixelformat,
-           format.fmt.pix.bytesperline);
-
-    return 1;
-}
-
-
-int vid_init() {
-    int i, j;
-
-    // TODO QUAA
-    vw = format.fmt.pix.width;
-    vh = format.fmt.pix.height;
-    vbytesperline = format.fmt.pix.bytesperline;
-
-    // Initialize vid_geo with the actual video dimensions
-    vid_geo.w = vw;
-    vid_geo.h = vh;
-    vid_geo.size = vw * vh;
-
-    xbytestep = xstep + xstep; // for YUV422. for other formats may differ
-    ybytestep = vbytesperline * (ystep-1);
-    // we shrink our pixels crudely, by hopping over them:
-    gw = vw / xstep;
-    gh = vh / ystep;
-    // aalib converts every block of 4 pixels to one character, so our sizes shrink by 2:
-    aw = gw / 2;
-    ah = gh / 2;
-
-    greysize = gw * gh;
-    grey = malloc(greysize); // To get grey from YUYV we simply ignore the U and V bytes
-    printf("Grey buffer is %i bytes\n", greysize);
-    for (j=0; j< 256; ++j)
-        YtoRGB[j] = 1.164*(j-256);
-
-    memset (&reqbuf, 0, sizeof (reqbuf));
-    reqbuf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    reqbuf.memory = V4L2_MEMORY_MMAP;
-    reqbuf.count = 32;
-
-    if (-1 == ioctl (fd, VIDIOC_REQBUFS, &reqbuf)) {
-        if (errno == EINVAL)
-            printf ("Fatal: Video capturing by mmap-streaming is not supported\n");
-        else
-            perror ("VIDIOC_REQBUFS");
-
-        exit (EXIT_FAILURE);
-    }
-    buffers = calloc (reqbuf.count, sizeof (*buffers));
-    if (buffers == NULL){
-        printf("calloc failure!");
-        exit (EXIT_FAILURE);
-    }
-
-    for (i = 0; i < reqbuf.count; i++) {
-
-        memset (&buffer, 0, sizeof (buffer));
-        buffer.type = reqbuf.type;
-	buffer.memory = V4L2_MEMORY_MMAP;
-        buffer.index = i;
-
-        if (-1 == ioctl (fd, VIDIOC_QUERYBUF, &buffer)) {
-            perror ("VIDIOC_QUERYBUF");
-            exit (EXIT_FAILURE);
-        }
-
-        buffers[i].length = buffer.length; /* remember for munmap() */
-
-        buffers[i].start = mmap (NULL, buffer.length,
-                                 PROT_READ | PROT_WRITE, /* recommended */
-                                 MAP_SHARED,             /* recommended */
-                                 fd, buffer.m.offset);
-
-        if (MAP_FAILED == buffers[i].start) {
-            /* If you do not exit here you should unmap() and free()
-             *                    the buffers mapped so far. */
-            perror ("mmap");
-            exit (EXIT_FAILURE);
-        }
-    }
-/* OK, the memory is mapped, so let's queue up the buffers,
-   next is: turn on streaming, and do the business. */
-
-    for (i = 0; i < reqbuf.count; i++) {
-        // queue up all the buffers for the first time
-        memset (&buffer, 0, sizeof (buffer));
-        buffer.type = reqbuf.type;
-	buffer.memory = V4L2_MEMORY_MMAP;
-        buffer.index = i;
-
-        if (-1 == ioctl (fd, VIDIOC_QBUF, &buffer)) {
-            perror ("VIDIOC_QBUF");
-            exit (EXIT_FAILURE);
-        }
-    }
-
-    // turn on streaming
-    if(-1 == ioctl(fd, VIDIOC_STREAMON, &buftype)) {
-	perror("VIDIOC_STREAMON");
-	exit(EXIT_FAILURE);
-    }
-
-
-    for (i = 0; i < greysize; i++) {
-	grey[i] = i % 160; //256;
-    }
-}
-
-
-void grab_one () {
-
-    // Can we have a buffer please?
-    if (-1 == ioctl (fd, VIDIOC_DQBUF, &buffer)) {
-        perror ("VIDIOC_DQBUF");
-        exit (EXIT_FAILURE);
-    }
-
-    if((++framenum) == renderhop){
-        framenum=0;
-        /* Get the ASCII context dimensions */
-        int ascii_width = aa_imgwidth(ascii_context);
-        int ascii_height = aa_imgheight(ascii_context);
-
-        /* Convert YUV to grey, scaling to match ASCII context size */
-        YUV422_to_grey_scaled(buffers[buffer.index].start, grey, vw, vh, ascii_width, ascii_height);
-
-        /* Copy grey data to ASCII context image buffer */
-        int grey_size = ascii_width * ascii_height;
-        int dest_size = aa_imgwidth(ascii_context) * aa_imgheight(ascii_context);
-        int copy_size = (grey_size < dest_size) ? grey_size : dest_size;
-        if (copy_size > 0) {
-            memcpy(aa_image(ascii_context), grey, copy_size);
-            aa_fastrender(ascii_context, 0, 0, ascii_width/2, ascii_height/2);
-        }
-        aa_flush(ascii_context);
-    }
-
-
-    // Thanks for lending us your buffer, you may have it back again:
-    if (-1 == ioctl (fd, VIDIOC_QBUF, &buffer)) {
-        perror ("VIDIOC_QBUF");
-        exit (EXIT_FAILURE);
-    }
-
-}
-
-#else
-int vid_detect(char *devfile) {
-    (void)devfile;
-    fprintf(stderr, "!! video capture is not implemented on this platform yet\n");
-    return -1;
-}
-
-int vid_init() {
-    return -1;
-}
-
-void grab_one () {
-}
-#endif
-
-
 
 void
 config_init (int argc, char *argv[]) {
@@ -662,9 +389,6 @@ config_init (int argc, char *argv[]) {
 
 int
 main (int argc, char **argv) {
-#if defined(HASCIICAM_HAVE_V4L2)
-    int i;
-#endif
 
     /* reminder:
        !!! grabbing height & width should be double
@@ -843,23 +567,9 @@ main (int argc, char **argv) {
 
   /* CLEAN EXIT */
 
-#if defined(HASCIICAM_HAVE_V4L2)
-  // turn off streaming
-  if(-1 == ioctl(fd, VIDIOC_STREAMOFF, &buftype)) {
-      perror("VIDIOC_STREAMOFF");
-  }
-
-/* Cleanup. */
-
-  for (i = 0; i < reqbuf.count; i++)
-      munmap (buffers[i].start, buffers[i].length);
-#endif
-
+  vid_close();
   aa_close(ascii_context);
   free(grey);
-#if defined(HASCIICAM_HAVE_V4L2)
-  if(fd>0) close(fd);
-#endif
   fprintf (stderr, "cya!\n");
   exit (0);
 /*++userbreak;*/
